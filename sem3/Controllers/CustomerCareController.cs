fea574f74e3e68bf84103c3a6dd677988c1cdf70
dxuan <duc47xuan@duc47xuangmail.7xuangmailcom>
2025-11-05 18:50:19 +0700

    update layout

--- sem3/Controllers/CustomerCareController.cs
+++ sem3/Controllers/CustomerCareController.cs
@@ -1,10 +1,13 @@
-﻿using System;
+﻿
+
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace sem3.Models.ModelViews
+namespace sem3.Controllers
 {
     public class CustomerCareController : Controller
     {
